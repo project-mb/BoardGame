f722e41df8d0211b0c6c945ba2eae46267799378
Begus001 <benjamin.goisser.bg@gmail.com>
2020-06-30 01:59:53 +0200

    Added TODOs to Main class

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+	public static ushort programState = 0;
+
+	void Start()
+	{
+		// TODO: Generate Board object and call generation method
+	}
+
+	void Update()
+	{
+		switch(programState)
+		{
+			default:
+				Debug.Log("Invalid programState"); // TODO: Handle error
+				break;
+		}
+	}
+}
