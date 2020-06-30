fa73bdbf987ded6e8887c455fc81ee9b25cae53b
MBGamesStudios <azeigs-github@yahoo.com>
2020-06-30 02:02:58 +0200

    Added GenerateNew Method

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Board : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public GameObject tile;
+	public GameObject cornerTile;
+	public static bool bla;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Start is called before the first frame update
+	void Awake()
+	{
+		Debug.Log("Instance Created");
+	}
+
+	public void GenerateNew(ushort width, ushort height)
+	{
+
+	}
+
+	//TODO: Generate from Load-File (? .json)
 }
