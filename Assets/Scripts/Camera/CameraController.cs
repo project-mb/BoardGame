3e9d51b8d164d51bda33318701c2bfb12248da12
MBGamesStudios <azeigs-github@yahoo.com>
2020-07-06 03:26:03 +0200

    Added UI Script with Menu-Method
    Added CameraRotaion and CameraZoom functionality

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CameraController : MonoBehaviour
+{
+	[Range(0.1f, 5)]
+	public float offset = 0.5f;
+
+	public Camera cam;
+	public GameObject pivot;
+
+	public float rotationSpeed = 250f;
+	public float zoomSensitivity = 0.75f;
+
+	private Vector3 startPos, endPos;
+	private float deltaX, deltaY;
+	private float dragDelta, rotDelta;
+
+	private Vector3 camDir;
+
+	private int interval = 10;
+
+	private void Update()
+	{
+		if (Input.GetMouseButton(Convert.ToInt32(MouseButton.LeftMouse)))
+		{
+			cam.transform.RotateAround(pivot.transform.position, Vector3.up, Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime);
+			//cam.transform.RotateAround(pivot.transform.position, Vector3.right, -Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime);
+			//cam.transform.localRotation = Quaternion.Euler(Quaternion.identity.x, Quaternion.identity.y, 0f);
+
+			//if (Time.frameCount % interval == 0)
+			//{
+			//	startPos = cam.ScreenToWorldPoint(Input.mousePosition);
+			//	Debug.Log(Input.mousePosition);
+			//}
+			//else if (Time.frameCount % interval == 59)
+			//{
+			//	endPos = cam.ScreenToWorldPoint(Input.mousePosition);
+			//	Debug.DrawRay(cam.transform.position, startPos, Color.blue);
+			//	Debug.Log(Input.mousePosition);
+			//	Debug.Log(startPos + "\n" + endPos);
+
+			//	//deltaX = Math.Abs(endPos.x - startPos.x);
+			//	//deltaY = Math.Abs(endPos.y - startPos.y);
+			//	//Debug.Log(deltaX + "\n" + deltaY);
+			//	//dragDelta = Vector3.Angle(startPos, endPos);
+			//	//Debug.LogWarning(dragDelta);
+			//}
+		}
+
+		if(Input.mouseScrollDelta.y > 0)
+		{
+			pivot.transform.position = new Vector3(pivot.transform.position.x, pivot.transform.position.y - zoomSensitivity, pivot.transform.position.z);
+		}
+		if (Input.mouseScrollDelta.y < 0)
+		{
+			pivot.transform.position = new Vector3(pivot.transform.position.x, pivot.transform.position.y + zoomSensitivity, pivot.transform.position.z);
+		}
+	}
+
+	private void OnMouseDrag()
+	{
+		Debug.Log("Hi there");
+	}
+
+	public void Centralize()
+	{
+		float xPos, yPos, zPos;
+
+		List<GameObject> cornerTiles = new List<GameObject>();
+
+		// Get CornerTiles
+		foreach (GameObject obj in Main.board.Tiles)
+		{
+			if (obj.GetComponent<Tile>().SpecificTile.IsCornerTile)
+			{
+				cornerTiles.Add(obj);
+			}
+		}
+
+		// Get Camera-xPos (middle of 2nd CornerTile - 1st CornerTile in xDirection)
+		xPos = (cornerTiles[3].transform.position.x - cornerTiles[0].transform.position.x) / 2;
+		// Get Camera-zPos (middle of 4th CornerTile - 1st CornerTile in zDirection)
+		zPos = (cornerTiles[1].transform.position.z - cornerTiles[0].transform.position.z) / 2;
+		// Get Camera-yPos acordingly to Board width and height
+		yPos = (Board.width > Board.height) ? Board.width : Board.height;
+
+		pivot.transform.position = new Vector3(xPos, offset, zPos);
+
+		cam.transform.localPosition = new Vector3(0f, yPos, 0f);
+	}
+}
---
+++ Assets/Scripts/GameController/UI.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+	public RawImage background;
+	public Button menu;
+	public Button resume;
+	public Button mainMenu;
+	public Button exit;
+
+	public static bool menuOpen;
+
+	// Start is called before the first frame update
+	void Awake()
+	{
+		menu.enabled = true;
+		menuOpen = false;
+
+		resume.enabled = false;
+		mainMenu.enabled = false;
+		exit.enabled = false;
+
+		background.color = new Color(0, 0, 0, 0f);
+
+		resume.GetComponentInChildren<Text>().color = new Color(
+			resume.GetComponentInChildren<Text>().color.r,
+			resume.GetComponentInChildren<Text>().color.g,
+			resume.GetComponentInChildren<Text>().color.b,
+			0f);
+		mainMenu.GetComponentInChildren<Text>().color = new Color(
+			mainMenu.GetComponentInChildren<Text>().color.r,
+			mainMenu.GetComponentInChildren<Text>().color.g,
+			mainMenu.GetComponentInChildren<Text>().color.b,
+			0f);
+		exit.GetComponentInChildren<Text>().color = new Color(
+			exit.GetComponentInChildren<Text>().color.r,
+			exit.GetComponentInChildren<Text>().color.g,
+			exit.GetComponentInChildren<Text>().color.b,
+			0f);
+
+		resume.image.color = new Color(resume.image.color.r, resume.image.color.g, resume.image.color.b, 0f);
+		mainMenu.image.color = new Color(mainMenu.image.color.r, mainMenu.image.color.g, mainMenu.image.color.b, 0f);
+		exit.image.color = new Color(exit.image.color.r, exit.image.color.g, exit.image.color.b, 0f);
+	}
+
+	public void Menu()
+	{
+		if (!menuOpen)
+		{
+			menuOpen = true;
+
+			resume.enabled = true;
+			mainMenu.enabled = true;
+			exit.enabled = true;
+
+			background.color = new Color(0, 0, 0, 0.5f);
+
+			resume.GetComponentInChildren<Text>().color = new Color(
+				resume.GetComponentInChildren<Text>().color.r,
+				resume.GetComponentInChildren<Text>().color.g,
+				resume.GetComponentInChildren<Text>().color.b,
+				1f);
+			mainMenu.GetComponentInChildren<Text>().color = new Color(
+				mainMenu.GetComponentInChildren<Text>().color.r,
+				mainMenu.GetComponentInChildren<Text>().color.g,
+				mainMenu.GetComponentInChildren<Text>().color.b,
+				1f);
+			exit.GetComponentInChildren<Text>().color = new Color(
+				exit.GetComponentInChildren<Text>().color.r,
+				exit.GetComponentInChildren<Text>().color.g,
+				exit.GetComponentInChildren<Text>().color.b,
+				1f);
+
+			resume.image.color = new Color(resume.image.color.r, resume.image.color.g, resume.image.color.b, 1f);
+			mainMenu.image.color = new Color(mainMenu.image.color.r, mainMenu.image.color.g, mainMenu.image.color.b, 1f);
+			exit.image.color = new Color(exit.image.color.r, exit.image.color.g, exit.image.color.b, 1f);
+		}
+		else
+		{
+			menuOpen = false;
+
+			resume.enabled = false;
+			mainMenu.enabled = false;
+			exit.enabled = false;
+
+			background.color = new Color(0, 0, 0, 0f);
+
+			resume.GetComponentInChildren<Text>().color = new Color(
+				resume.GetComponentInChildren<Text>().color.r,
+				resume.GetComponentInChildren<Text>().color.g,
+				resume.GetComponentInChildren<Text>().color.b,
+				0f);
+			mainMenu.GetComponentInChildren<Text>().color = new Color(
+				mainMenu.GetComponentInChildren<Text>().color.r,
+				mainMenu.GetComponentInChildren<Text>().color.g,
+				mainMenu.GetComponentInChildren<Text>().color.b,
+				0f);
+			exit.GetComponentInChildren<Text>().color = new Color(
+				exit.GetComponentInChildren<Text>().color.r,
+				exit.GetComponentInChildren<Text>().color.g,
+				exit.GetComponentInChildren<Text>().color.b,
+				0f);
+
+			resume.image.color = new Color(resume.image.color.r, resume.image.color.g, resume.image.color.b, 0f);
+			mainMenu.image.color = new Color(mainMenu.image.color.r, mainMenu.image.color.g, mainMenu.image.color.b, 0f);
+			exit.image.color = new Color(exit.image.color.r, exit.image.color.g, exit.image.color.b, 0f);
+		}
+	}
+}
